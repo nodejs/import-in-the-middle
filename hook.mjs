6d321f27172c0fb5d0b8ece99f95db5fa7699a2b
Bryan English <bryan.english@datadoghq.com>
2021-08-11 18:14:38 -0400

    add license info to every file

--- hook.mjs
+++ hook.mjs
@@ -1,3 +1,7 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2.0 License.
+//
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2021 Datadog, Inc.
+
 export async function resolve (specifier, context, parentResolve) {
   const { parentURL = '' } = context
 
--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,7 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2.0 License.
+//
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2021 Datadog, Inc.
+
 /**
  * A hook function to be run against loaded modules.
  * @param {url} string The absolute path of the module, as a `file:` URL string.
