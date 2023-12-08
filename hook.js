88d15b6d450004d513b1cdcf43a4da0320dffd0b
James Sumners <jsumners@newrelic.com>
2023-12-08 09:28:48 -0500

    Fix crypto require

--- hook.js
+++ hook.js
@@ -2,7 +2,7 @@
 //
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2021 Datadog, Inc.
 
-const { randomBytes } = require('node:crypto')
+const { randomBytes } = require('crypto')
 const specifiers = new Map()
 const isWin = process.platform === "win32"
 
