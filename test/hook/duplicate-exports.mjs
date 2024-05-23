52c5ac5a8d20c51a99d8843f759468dd5862e7c4
Tim Fish <tim@timfish.uk>
2024-05-23 23:19:05 +0200

    Test for which export takes priority

--- test/hook/duplicate-exports.mjs
+++ test/hook/duplicate-exports.mjs
@@ -4,6 +4,8 @@ import { strictEqual } from 'assert'
 
 Hook((exports, name) => {
   if (name.endsWith('/duplicate.mjs')) {
+    // The last export always takes priority
+    strictEqual(exports.foo, 'b')
     exports.foo = '1'
   }
 })
