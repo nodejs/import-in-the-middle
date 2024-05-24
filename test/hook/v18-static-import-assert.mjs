568bd27660050d169710e3375e1c7ec2e98ae05a
RafaelGSS <rafael.nunu@hotmail.com>
2024-05-24 15:32:18 -0300

    test: skip static-import on v22

--- test/hook/v18-static-import-assert.mjs
+++ test/hook/v18-static-import-assert.mjs
@@ -6,6 +6,13 @@ import Hook from '../../index.js'
 import jsonMjs from '../fixtures/json.mjs'
 import { strictEqual } from 'assert'
 
+// TODO: remove this test case when v18/v20 is EOL
+// This test fails on v22 >=
+if (process.version.startsWith('v22')) {
+  console.log('skipping v18-static-import-assert.mjs as this is Node.js v22 and test wants <= v21.x')
+  process.exit(0)
+}
+
 Hook((exports, name) => {
   if (name.match(/json\.mjs/)) {
     exports.default.data += '-dawg'
