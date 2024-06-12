08c68777e69ce4df41255dfdc4b3edc71c3f831d
Tim Fish <tim@timfish.uk>
2024-06-13 00:55:11 +0200

    change failing test

--- test/other/import-executable.mjs
+++ test/other/import-executable.mjs
@@ -2,18 +2,7 @@
 //
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2021 Datadog, Inc.
 
-import { rejects } from 'assert'
 (async () => {
-  const [processMajor, processMinor] = process.versions.node.split('.').map(Number)
-  const extensionlessSupported = processMajor >= 21 ||
-    (processMajor === 20 && processMinor >= 10) ||
-    (processMajor === 18 && processMinor >= 19)
-  if (extensionlessSupported) {
-    // Files without extension are supported in Node.js ^21, ^20.10.0, and ^18.19.0
-    return
-  }
-  await rejects(() => import('./executable'), {
-    name: 'TypeError',
-    code: 'ERR_UNKNOWN_FILE_EXTENSION'
-  })
+  const lib = await import('../fixtures/executable')
+  console.assert(lib)
 })()
