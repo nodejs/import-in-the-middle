d82c8ae8529baa13f5878dc61f211044886f43ee
Tim Fish <tim@timfish.uk>
2024-07-17 01:50:24 +0200

    debug failing CI

--- test/register/v18.19-include-message-port.mjs
+++ test/register/v18.19-include-message-port.mjs
@@ -4,4 +4,11 @@ const out = spawnSync(process.execPath,
   ['--import', './test/fixtures/import.mjs', './test/fixtures/import-after.mjs'],
   { stdio: 'inherit', env: {} }
 )
+
+if (out.error) {
+  console.error(out.error)
+}
+if (out.status !== 0) {
+  console.error(out.stderr.toString())
+}
 process.exit(out.status)
