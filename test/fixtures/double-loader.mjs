2973a647e9a1ebda762b5345738de14b1830f1e4
Tim Fish <tim@timfish.uk>
2025-09-25 13:20:11 +0200

    Revert "fix: use `createRequire` to load `hook.js` (#205)"
    This reverts commit 81a2ae0ea094df27c9baaf6e267276e0acb21af1.

--- test/fixtures/double-loader.mjs
+++
@@ -1,9 +0,0 @@
-import { readFile } from 'fs/promises'
-
-export async function load (url, context, nextLoad) {
-  const result = await nextLoad(url, context)
-  if (!result.source && url.startsWith('file:')) {
-    result.source = await readFile(new URL(url))
-  }
-  return result
-}
