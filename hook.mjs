cc116ce5bd91d32653aee6276deca32d9cb9c488
Tim Fish <tim@timfish.uk>
2025-09-25 11:51:33 +0200

    comment

--- hook.mjs
+++ hook.mjs
@@ -4,6 +4,8 @@
 
 import * as module from 'node:module'
 
+// Using 'module.createRequire' is key for this to be resolved by @vercel/nft:
+// https://github.com/vercel/nft/pull/459/files#diff-f9de9e832021754e4f7e493614502e6a12c6adf1d75f666ffdf9c2b8de8d33bcR1090
 const require = module.createRequire(import.meta.url)
 const { createHook } = require('./hook.js')
 
