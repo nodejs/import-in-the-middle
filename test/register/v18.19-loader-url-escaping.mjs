3d565ad645040a913f49ef2a95f237c5bc02122a
Ruben Bridgewater <ruben.bridgewater@datadoghq.com>
2026-07-17 13:54:09 +0200

    chore: remove test license header

--- test/register/v18.19-loader-url-escaping.mjs
+++ test/register/v18.19-loader-url-escaping.mjs
@@ -1,7 +1,3 @@
-// Unless explicitly stated otherwise all files in this repository are licensed under the Apache 2.0 License.
-//
-// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2026 Datadog, Inc.
-
 import { strictEqual } from 'node:assert/strict'
 import { spawnSync } from 'node:child_process'
 import { copyFile, mkdir, mkdtemp, rm, writeFile } from 'node:fs/promises'
